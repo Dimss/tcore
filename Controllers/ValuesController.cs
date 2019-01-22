53de30530ca02df3e1fb27875b29d3cab110b165
Dmitry Kartsev <dkartsev@redhat.com>
2019-01-22 20:32:28 +0200

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TCore.Models;
+using Newtonsoft.Json;
+
+namespace TCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+
+        // GET api/items/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            Item i = new Item();
+            i.Id = id;
+            i.ItemName = "ItemX";
+            i.ItemType = "SecretItem";
+            
+            return JsonConvert.SerializeObject(i);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,9 @@
+namespace TCore.Models
+{
+  public class Item{
+    public long Id {get;set;}
+    public string ItemName {get;set;}
+    public string ItemType {get;set;}
+
+  }
+}
